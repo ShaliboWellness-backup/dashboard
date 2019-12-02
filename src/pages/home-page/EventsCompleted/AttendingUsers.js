a7a27a1fd89722414eb6412dd9bb03c877ab886d
Roy <royzarchi@gmail.com>
2019-12-02 11:51:13 +0200

    Displaying past events and fixed promotion dialog bug

---
+++ src/pages/home-page/EventsCompleted/AttendingUsers.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import Checkbox from '@material-ui/core/Checkbox'
+import {ExpansionPanelActions, Switch} from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import {useApolloClient} from "@apollo/react-hooks";
+import updateEventMutation from '../../../graphql/event/mutation/update-event';
+import verifyEventUsersMutation from "../../../graphql/event/mutation/verify-event-users";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        boxShadow: 'none',
+        border: 'none',
+        marginTop: 16
+    },
+    table: {
+        marginTop: 0,
+
+
+    },
+    tableHead: {
+        fontWeight: 700,
+        color: "#222a42b3",
+        fontSize: "0.875rem"
+    },
+    tableBody: {
+        fontWeight: 400,
+        color: "#222a42b3",
+        fontSize: "0.875rem",
+
+    },
+    message: {
+
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+}));
+
+export default function AttendingUsers({event, users, children}) {
+    const classes = useStyles();
+
+    const client = useApolloClient()
+    
+    return (
+        <ExpansionPanel classes={{root: classes.root}}>
+            <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon/>}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+            >
+                <Typography variant={"subtitle2"} className={classes.heading}>Attending Members</Typography>
+
+            </ExpansionPanelSummary>
+            <ExpansionPanelActions style={{display: 'flex', justifyContent: 'flex-start'}}>
+                {children}
+            </ExpansionPanelActions>
+            <ExpansionPanelDetails style={{minHeight: 125}}>
+                {users.length > 0 ?
+                    <Table className={classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className={classes.tableHead}>Name</TableCell>
+                                <TableCell className={classes.tableHead}>Email</TableCell>
+                                <TableCell className={classes.tableHead}>Attended</TableCell>
+                                <TableCell className={classes.tableHead}>Remove</TableCell>
+
+                                {/*<TableCell className={classes.tableHead}>Company</TableCell>*/}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {users.length > 0 &&
+
+                            users.map(user => {
+                                const {_id} = user
+                                let oldVerifiedUsers = event.verifiedUsers.map((user) => user._id)
+                                return (
+                                    <TableRow key={user._id}>
+                                        <TableCell
+                                            className={classes.tableBody}>{user.firstName} {user.lastName}</TableCell>
+                                        <TableCell className={classes.tableBody}>{user.email}</TableCell>
+                                        <TableCell className={classes.tableBody}>
+                                            <Checkbox
+                                                color={"primary"}
+                                                defaultChecked={oldVerifiedUsers.includes(_id)}
+                                                onChange={() => {
+                                                    let newVerifiedUsers = oldVerifiedUsers.includes(_id) ?
+                                                        oldVerifiedUsers.filter((userId) => userId !== _id)
+                                                        :
+                                                        [...oldVerifiedUsers, _id]
+
+                                                    client.mutate({
+                                                        mutation: verifyEventUsersMutation,
+                                                        variables:
+                                                            {
+                                                                _id: event._id,
+                                                                usersIds: newVerifiedUsers
+                                                            }
+                                                    })
+                                                        .then((data) => {
+                                                            console.log(data)
+
+                                                        })
+                                                        .catch((error) => {
+                                                            console.log(error)
+                                                        })
+
+                                                }}
+                                                value="checkedA"
+                                                inputProps={{
+                                                    'aria-label': 'primary checkbox',
+                                                }}
+                                            />
+                                        </TableCell>
+                                        <TableCell className={classes.tableBody}>
+                                            <Button onClick={() => {
+                                                const updatedUsers = event.users.filter((value) => value._id !== user._id);
+                                                client.mutate({
+                                                    mutation: updateEventMutation,
+                                                    variables: {
+                                                        _id: event._id,
+                                                        users: updatedUsers.map((user) => user._id)
+                                                    }
+                                                }).then(value => {
+                                                    console.log('Success')
+                                                });
+                                            }} color="primary">
+                                                Remove
+                                            </Button>
+                                        </TableCell>
+
+                                    </TableRow>
+                                )
+                            })}
+                        </TableBody>
+                    </Table>
+                    : <div className={classes.message}>
+                        <Typography variant={'body1'}>
+                            There are no active users for this event.
+                        </Typography>
+                    </div>}
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+
+    );
+}
---
+++ src/pages/home-page/EventsCompleted/index.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import {Grid, Typography, Paper, CardMedia} from '@material-ui/core';
+import EventCard from './EventCard';
+import CreateDialog from '../../../components/common/CreateDialog';
+import logo from '../../../Assets/logo.png'
+import moment from 'moment'
+import CurrentCompanyContext from "../../../containers/CurrentCompany/CurrentCompanyContext";
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 650,
+    },
+    empty: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: "8px 8px",
+        height: 400,
+        flex: 1,
+
+    },
+    emptyText: {
+        fontWeight: 400,
+        color: '#75D6B2',
+        marginTop: 16
+    },
+    header: {
+        display: "flex",
+        textAlign: "center",
+        width: "100%",
+        marginBottom: 32,
+        justifyContent: "center",
+        position: "relative"
+    },
+
+});
+
+const Events = ({disableCreateEvent, classes, events}) => {
+
+    const {currentCompany} = React.useContext(CurrentCompanyContext)
+
+    let futureEvents = events.filter(event => moment(event.date).isAfter(moment().startOf("day")))
+    let sortedEvents = [
+        {
+            startOfWeek: moment().startOf('week'),
+            endOfWeek: moment().endOf('week'),
+            events: []
+        }]
+    futureEvents.map((event) => {
+        // check if event belongs to existing week
+        //If so, add event to week
+        let i
+        let addedToWeek = false
+        for (i = 0; i < sortedEvents.length; i++) {
+            const {startOfWeek, endOfWeek} = sortedEvents[i]
+            if (moment(event.date).isBetween(startOfWeek, endOfWeek)) {
+                sortedEvents[i].events.push(event)
+                addedToWeek = true
+            } else {
+
+            }
+        }
+        //if event was not added to any week, create new week and add current event to it
+        if (addedToWeek === true) {
+            return null
+        } else {
+            let startOfWeek = moment(event.date).startOf('week')
+            let endOfWeek = moment(event.date).endOf('week')
+            let newWeek = {startOfWeek, endOfWeek, events: [event]}
+            sortedEvents.push(newWeek)
+        }
+
+    })
+    //sort the events by date within week
+    let sortedByDate = sortedEvents.map((weeklyEventsArray) => {
+        let sortedEvents = weeklyEventsArray.events.sort(function (a, b) {
+            return moment(b.date).isAfter(a.date) ? -1 : moment(a.date).isAfter(b.date) ? 1 : 0
+        });
+        return {...weeklyEventsArray, events: sortedEvents}
+    })
+
+    return (
+        <div className={classes.grid}>
+            {!disableCreateEvent && <div className={classes.header}>
+                <CreateDialog type="event" action={"create"}/>
+                <Typography gutterBottom variant={"h4"}>
+                    {`${!!currentCompany && currentCompany.name} Events`}
+                </Typography>
+            </div>}
+            {sortedByDate[0].events.length > 0 && <Typography gutterBottom variant={"h5"}
+                                                              style={{
+                                                                  paddingLeft: 8,
+                                                                  borderBottom: '1px solid black',
+                                                                  borderColor: '#00f2c3'
+                                                              }}>
+                This Week
+            </Typography>
+            }
+            <Grid container spacing={2} style={{marginBottom: 24, marginTop: 8}}>
+                {futureEvents.length < 1 && !disableCreateEvent &&
+                <Paper className={classes.empty}>
+                    <div style={{textAlign: 'center '}}>
+                        <CardMedia style={{width: 200, height: 200, margin: 'auto'}} image={logo}/>
+                        <Typography variant={"h4"} className={classes.emptyText}>
+                            There are no events for this company
+                        </Typography>
+                    </div>
+                </Paper>}
+                {futureEvents.length < 1 && disableCreateEvent ?
+                    <Paper className={classes.empty}>
+                        <div style={{textAlign: 'center '}}>
+                            <CardMedia style={{width: 200, height: 200, margin: 'auto'}} image={logo}/>
+                            <Typography variant={"h4"} className={classes.emptyText}>
+                                No events are assigned to you at this time
+                            </Typography>
+                        </div>
+                    </Paper> :
+                    sortedByDate[0].events.map((event, index) => (
+                        <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
+                            <EventCard
+                                event={event}
+                                title={event.title}
+                                instructor={event.instructor}
+                                location={event.location}
+                                date={event.date}
+                                totalSpots={event.totalSpots}
+                                takenSpots={event.users.length}
+                                description={event.description}
+                                image={event.image}
+                                id={event.id}
+                            />
+                        </Grid>
+                    ))}
+            </Grid>
+            {sortedByDate.slice(1).map((week, index) => (
+                <div style={{marginBottom: 24}}>
+                    <Typography gutterBottom variant={"h5"}
+                                style={{
+                                    paddingLeft: 8,
+                                    borderBottom: '1px solid black',
+                                    borderColor: '#00f2c3'
+                                }}>
+                        {moment().diff(week.startOfWeek, 'weeks') === 0 ? 'Next Week' : `${-moment().diff(week.startOfWeek, 'weeks') + 1} Weeks From Now`}
+                    </Typography>
+                    <Grid container spacing={2} style={{
+                        marginTop: 8,
+                    }}>
+                        {week.events.map((event, index) => (
+                            <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
+                                <EventCard
+                                    event={event}
+                                    title={event.title}
+                                    instructor={event.instructor}
+                                    location={event.location}
+                                    date={event.date}
+                                    totalSpots={event.totalSpots}
+                                    takenSpots={event.users.length}
+                                    description={event.description}
+                                    image={event.image}
+                                    id={event.id}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </div>
+            ))}
+
+
+        </div>
+    );
+
+}
+
+export default withStyles(styles)(Events);
